0a5d34b43ed2a21f685f0b0a2eccb41083b4bd25
Urso-Suzi <pedroviniciussouza24@gmail.com>
2024-05-28 21:17:13 -0300

    emplementação de animação

--- scrpt.js
+++ scrpt.js
@@ -1,8 +1,26 @@
+let px=0
+let py=0
+
+let anima 
+
+const desenha = function () {
 let canvas = document.getElementById("desenho1")
 let ctx = canvas.getContext('2d')
 
-ctx.fillStyle = "#00f"
-ctx.fillRect(0,0,100,100)
+if(px == 480) {
+    px = 0 
+    py+=50
+} else if (py > 450) {
+    py = 0
+    px = 0
+    ctx.clearRect(0,0,500,500)
+}
 
-ctx.fillStyle = "rgba(255,0,0,0.5)"
-ctx.fillRect(50,50,100,100)
+ctx.fillStyle ='#f00'
+ctx.fillRect(px,py,50,50)
+px+=10
+
+
+anima = requestAnimationFrame(desenha)
+
+}
