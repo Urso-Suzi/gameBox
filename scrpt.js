3c3cfbd566c760e49513e3aa3ab9b92395d4400a
Urso-Suzi <pedroviniciussouza24@gmail.com>
2024-05-29 21:48:05 -0300

    aprendendo animações

--- scrpt.js
+++ scrpt.js
@@ -1,26 +1,43 @@
 let px=0
 let py=0
-
+let dx=5
+let dy=0
+ 
 let anima 
+let l_ret = a_ret=50
+let l_canvas = a_canvas = 500
 
-const desenha = function () {
+function desenha () {
 let canvas = document.getElementById("desenho1")
 let ctx = canvas.getContext('2d')
 
-if(px == 480) {
-    px = 0 
-    py+=50
-} else if (py > 450) {
-    py = 0
-    px = 0
-    ctx.clearRect(0,0,500,500)
-}
+ctx.clearRect(0,0,l_canvas, a_canvas)
 
 ctx.fillStyle ='#f00'
-ctx.fillRect(px,py,50,50)
-px+=10
+ctx.lineWidth = 5
+ctx.stokeStyle = '#000'
+ctx.strokeRect(px,py,l_ret,a_ret)
+ctx.fillRect(px,py,l_ret,a_ret)
 
+px+=dx
+py+=dy
 
-anima = requestAnimationFrame(desenha)
-
+if((px <= 0) && (py<= 0)){
+    dx=5
+    dy=0
+} if((px+l_ret >= l_canvas) && (py <= 0)){
+    dx=0
+    dy=5
+} if((px+l_ret >= l_canvas) && (py+a_ret >= a_canvas)){
+    dx=-5
+    dy=0
+} if((px <= 0) && (py+a_ret >= a_canvas)) {
+    dx = 0
+    dy =-5
 }
+
+
+anima=requestAnimationFrame(desenha)
+}
+
+// desenha()
